39132341362b29161545cd49a607027b1db85b58
Scognamiglio <loic.scognamiglio16@gmail.com>
2023-09-10 02:06:07 +0200

    feat(core) : Permet d'avoir plusieurs instance dans un même projet

--- src/MoreClassChildren.php
+++ src/MoreClassChildren.php
@@ -5,21 +5,25 @@ class MoreClassChildren {
     private static $architect = [];
     private static $trace = false;
     private static $config = [];
+    private $id = 0;
+    private static $lastId = 0;
     function __construct(...$class){
+        $this->id = self::$lastId;
         foreach ($class as $instance){
             $refClass = &$instance;
             $listMethods = get_class_methods($instance);
             array_filter($listMethods,function ($m){
                 return strpos($m,'_') !== 0;
             });
-            self::$architect = array_merge(self::$architect,array_fill_keys($listMethods,$refClass));
+            self::$architect[$this->id] = array_merge(self::$architect[$this->id] ?? [],array_fill_keys($listMethods,$refClass));
         }
+        self::$lastId++;
     }
 
     function __call($name, $arguments)
     {
-        if(!empty(self::$architect[$name])){
-            $refClass = self::$architect[$name];
+        if(!empty(self::$architect[$this->id][$name])){
+            $refClass = self::$architect[$this->id][$name];
             if(self::$trace){
                 $this->sendTrace($name,$refClass,$arguments);
             }
@@ -41,7 +45,12 @@ class MoreClassChildren {
         self::$trace = $trace;
     }
 
-
+    function getConfigInstance($name){
+        return self::$config[$this->id][$name] ?? self::$config[$name] ?? null;
+    }
+    function setConfigInstance($name,$value){
+        self::$config[$this->id][$name] = $value;
+    }
     static function getConfig($name){
         return self::$config[$name] ?? null;
     }
